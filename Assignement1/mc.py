701fb83e5e98244d2e21ba0a1c850f808acb35a2
Lisa Infantino <Lisa.infantino@student.uliege.be>
2025-10-22 21:28:09 +0200

    mc output txt

--- Assignement1/mc.py
+++ Assignement1/mc.py
@@ -50,19 +50,33 @@ def averageScore(sample_size, ls_size, nb_tests, method, hyperparameter, dataset
     print("\nmethod ", method, ": \nAverage score ", score_avg, "\nStandard deviation ", score_std)
     return score_avg, score_std
 
+def writeFileResults(filename, results, lines):
+    f = open(filename, 'w')
+    results_str = "Results:\n"
+    m, n = np.shape(results)
+    for i in range(m):
+        results_str += lines[i] + " & "
+        for j in range(n):
+            if j == n - 1:
+                results_str += str(results[i][j]) + " \\\ \n\hline\n"
+            else:
+                results_str += str(results[i][j]) + " & "
+    f.write(results_str)
+
 def kFoldCrossValidation(attributes, classes, method, k=2):
     if len(classes)%k != 0:
         print("k must divide the dataset size")
         return -1
     split_size = int(len(classes) / k)
+    if method == "dt":
+        hp_values = [1, 2, 4, 6, None]
+    elif method == "knn":
+        hp_values = [1, 2, 25, 125, 500, 899]
+    hyperparam = 0
     max_score = 0
-    score = 0
-    hyperparam = 1
     # test hyperparameter values until it stops increasing accuracy
-    while(score >= max_score):
-        max_score = score
+    for p in hp_values:
         score = 0
-        hyperparam += 1
         for i in range(k):
             # use each of the k subset as a pseudo test sample and compute the average accuracy
             slice_start = split_size * i
@@ -70,11 +84,13 @@ def kFoldCrossValidation(attributes, classes, method, k=2):
             test_subset = [attributes[slice_start:slice_end], classes[slice_start:slice_end]]
             ls = [np.append(attributes[:slice_start], attributes[slice_end:], axis=0), np.append(classes[:slice_start], classes[slice_end:])]
             if method == "dt":
-                score += testDecisionTree(ls, test_subset, max_depth=hyperparam)
+                score += testDecisionTree(ls, test_subset, max_depth=p)
             elif method == "knn":
-                score += testKNN(ls, test_subset, k=hyperparam)
+                score += testKNN(ls, test_subset, k=p)
         score /= k
-    print("method ", method, ": ", max_score, "accuracy for hyperparameter = ", hyperparam)
+        if score > max_score:
+            max_score = score
+            hyperparam = p
     return hyperparam
 
 if __name__ == "__main__":
@@ -84,31 +100,36 @@ if __name__ == "__main__":
     LS_SIZE2 = 421
     NB_TESTS = 5
 
+    results = np.zeros((2, 4))
+    
+
     # c'est moche mais j'en reste là pour aujourd'hui, c'est temporaire
     # Dataset1 : 
-    print("----------Dataset 1 :----------\n")
-    x, y = make_dataset1(SAMPLE_SIZE1, 1234)
+    print("----------Dataset 1 :----------")
+    x, y = make_dataset1(SAMPLE_SIZE1, 0)
     x_train = x[:LS_SIZE1]
     y_train = y[:LS_SIZE1]
     x_test = x[LS_SIZE1:]
     y_test = y[LS_SIZE1:]
     # tune hyperparams
-    max_depth = kFoldCrossValidation(x_train, y_train, "dt", 60)
-    k = kFoldCrossValidation(x, y, "knn", 60)
+    max_depth = kFoldCrossValidation(x_train, y_train, "dt", 6)
+    k = kFoldCrossValidation(x, y, "knn", 6)
     # accuracies
-    averageScore(SAMPLE_SIZE1, LS_SIZE1, NB_TESTS, "dt", max_depth, dataset=1)
-    averageScore(SAMPLE_SIZE1, LS_SIZE1, NB_TESTS, "knn", k, dataset=1)
+    results[0][0], results[1][0] = averageScore(SAMPLE_SIZE1, LS_SIZE1, NB_TESTS, "dt", max_depth, dataset=1)
+    results[0][1], results[1][1] = averageScore(SAMPLE_SIZE1, LS_SIZE1, NB_TESTS, "knn", k, dataset=1)
 
     # Dataset2 :
-    print("----------Dataset 2 :----------\n")
+    print("\n----------Dataset 2 :----------")
     x, y = make_dataset1(SAMPLE_SIZE1, 1234)
     x_train = x[:LS_SIZE1]
     y_train = y[:LS_SIZE1]
     x_test = x[LS_SIZE1:]
     y_test = y[LS_SIZE1:]
     # tune hyperparams
-    max_depth = kFoldCrossValidation(x_train, y_train, "dt", 60)
-    k = kFoldCrossValidation(x, y, "knn", 60)
+    max_depth = kFoldCrossValidation(x_train, y_train, "dt", 6)
+    k = kFoldCrossValidation(x, y, "knn", 6)
     # accuracies
-    averageScore(SAMPLE_SIZE2, LS_SIZE2, NB_TESTS, "dt", max_depth, dataset=2)
-    averageScore(SAMPLE_SIZE2, LS_SIZE2, NB_TESTS, "knn", k, dataset=2)
+    results[0][2], results[1][2] = averageScore(SAMPLE_SIZE2, LS_SIZE2, NB_TESTS, "dt", max_depth, dataset=2)
+    results[0][3], results[1][3] = averageScore(SAMPLE_SIZE2, LS_SIZE2, NB_TESTS, "knn", k, dataset=2)
+    print("\nresults :\n", results)
+    writeFileResults("mc_results.txt", results, ["Average accuracies", "Standard deviation"])
